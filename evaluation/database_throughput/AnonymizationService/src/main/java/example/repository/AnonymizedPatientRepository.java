4d242c2eafe2ccbb9676d9a9ebd69f6ee90c29f5
ingastrelnikova <ingastrelnikova@yahoo.com>
2024-08-01 22:49:19 +0200

    fixes

--- evaluation/database_throughput/AnonymizationService/src/main/java/example/repository/AnonymizedPatientRepository.java
+++
@@ -1,7 +0,0 @@
-package example.repository;
-
-import example.entity.AnonymizedPatient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnonymizedPatientRepository extends JpaRepository<AnonymizedPatient, Long> {
-}
